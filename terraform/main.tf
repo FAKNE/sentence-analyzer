432b15d38e4f0fca8254c320ea83da6bdfcdaf44
Mario Manuel <juniormariodev@gmail.com>
2025-08-19 11:51:42 +0200

    chore(ci): integrate Terraform into CI/CD workflow for infrastructure automation

---
+++ terraform/main.tf
@@ -0,0 +1,48 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+
+  required_version = ">= 1.5.0"
+}
+
+
+resource "aws_security_group" "allow_ssh_http" {
+  name        = "allow_ssh_http"
+  description = "Allow SSH and HTTP"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "k3s_node" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  key_name      = var.key_name
+  vpc_security_group_ids = [aws_security_group.allow_ssh_http.id]
+
+  tags = {
+    Name = "k3s-node"
+  }
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,16 @@
+variable "aws_region" {
+  default = "us-east-1"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "ami_id" {
+  default = "ami-0c94855ba95c71c99"
+}
+
+variable "key_name" {
+  description = "AWS Key Pair name for SSH access"
+  default     = "sentence-analyzer-key-pair"
+}
